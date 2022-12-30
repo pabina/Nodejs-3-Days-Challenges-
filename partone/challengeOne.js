ccb04f15c50486d389509e101a4da699d3ec170e
pabina <rimalpabina1234@gmail.com>
2022-12-30 10:10:15 +0545

    challenge two

--- partone/challengeOne.js
+++ partone/challengeOne.js
@@ -1,3 +1,27 @@
 const fs=require("fs");
 fs.writeFileSync("first.txt","hello pabina how are you");
 fs.appendFileSync("first.txt"," this content should be added");
+
+
+
+
+//for video 2
+// const myname="pabina rimal";
+// console.log("hello pabina");
+// const myfun=(a,b)=>{
+// return a+b
+// }
+// module.exports=myname;
+// module.exports=myfun;
+// module.exports={myname,myfun}
+
+
+
+// for challenge 2
+const mynote=()=>{
+    // buffer.toString("utf-8", 0, 12)
+    let myfile=fs.readFileSync("first.txt");
+    return myfile.toString();
+}
+
+module.exports =mynote;
---
+++ partone/challengeTwo.js
@@ -0,0 +1,13 @@
+// let name=require("./challengeOne");
+// let {myname,myfun}=require("./challengeOne");
+let mynote=require ("./challengeOne");
+
+
+//from video
+// console.log(myname);
+// console.log(myfun(4,5));
+
+
+// for challenge
+console.log(mynote());
+

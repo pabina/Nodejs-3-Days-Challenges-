79f01ce126b80d1af60d0e76a8a3d9b8bda1ffdb
pabina <rimalpabina1234@gmail.com>
2022-12-31 00:47:36 +0545

    challenge 5 by own

--- partone/challengeFive.js
+++ partone/challengeFive.js
@@ -1,10 +1,37 @@
-const book={
-    title:"titleone",
-    author:"authorone"
+let fs=require('fs');
+let yargs=require("yargs");
+const mydata={
+    name:"pabina",
+    age:24
 }
+const mydataJson=JSON.stringify(mydata)
+const myrealdata=JSON.parse(mydataJson)
 
-const bookJson=JSON.stringify(book)
-const bookData=JSON.parse(bookJson)
 
-console.log(bookJson);
-console.log(bookData);
+
+yargs.command({
+    command:"add",
+    describe:"add new info",
+    builder:{
+        name:{
+            describe:"name is required field",
+           demandOption:"true",
+           type:"string",
+        },
+        age:{
+            describe:"age is required field",
+          demandOption:"true",
+          type:"number",
+        }
+    },
+    handler:function(argv){
+       const myInfoData={name:argv.name,age:argv.age}
+      const myinfoJsonData=JSON.stringify(myInfoData);
+     
+      fs.writeFileSync("mynote.json",myinfoJsonData);
+    const res= (fs.readFileSync("mynote.json")).toString();
+    console.log(res);
+    }
+})
+
+yargs.parse();

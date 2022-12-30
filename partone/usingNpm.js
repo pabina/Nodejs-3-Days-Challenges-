6dc95dd94f67fcc32ac4212a50e4d847a6014c02
pabina <rimalpabina1234@gmail.com>
2022-12-30 14:49:41 +0545

    challenge3 added

--- partone/usingNpm.js
+++ partone/usingNpm.js
@@ -1,5 +1,15 @@
-const { getMaxListeners } = require("process");
-const validator=require("validator");
+// const { getMaxListeners } = require("process");
+// const validator=require("validator");
 
 
-console.log(validator.isEmail("rimal@gmail.com"));
+// console.log(validator.isEmail("rimal@gmail.com"));
+
+
+//for challenge 3
+const chalk=require("chalk");
+
+console.log(chalk.red("hello pabina"));
+console.log(chalk.red(chalk.bold("hello pabina")));
+console.log(chalk.blue(chalk.inverse(chalk.bold("hello pabina"))));
+
+

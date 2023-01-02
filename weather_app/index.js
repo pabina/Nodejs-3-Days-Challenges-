860374622b140699f6935cf73053a6e0a3839a71
pabina <rimalpabina1234@gmail.com>
2023-01-02 15:53:51 +0545

    simple error hanldling for http request

--- weather_app/index.js
+++ weather_app/index.js
@@ -1,14 +1,30 @@
 const request=require("request");
 
 const url="https://api.weatherapi.com/v1/current.json?key=13e99e5cd4424c5b8c374814230201&q=nepal"
+// const url="https://api.weatherapi.com/v1/current.json?key=13e99e5cd4424c5b8c374814230201"
 
 
 
-//challenge one
+
+//challenge two
 request({url:url,json:true},(error,response)=>{
-    const data=response.body.current;
-    console.log(`It is currently ${data.temp_c} degrees out.And feels like ${data.feelslike_c} degrees `)
+    if(error){
+console.log("unable to connection to location services")
+     }
+     else if(response.body.current===""){
+ console.log("Unalble to find location try another search")
+     }
+    else{
+        const data=response.body.current;
+        console.log(`It is currently ${data.temp_c} degrees out.And feels like ${data.feelslike_c} degrees `)
+    }
+    
 })
+
+
+//challenge two geo
+
+
 
 
 

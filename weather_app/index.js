20b5b867cf2c88a770c56035eb0b9e727d7d6a2c
pabina <rimalpabina1234@gmail.com>
2023-01-02 15:04:14 +0545

    http challenge_one done

--- weather_app/index.js
+++ weather_app/index.js
@@ -3,7 +3,12 @@ const request=require("request");
 const url="https://api.weatherapi.com/v1/current.json?key=13e99e5cd4424c5b8c374814230201&q=nepal"
 
 
-request({url:url},(error,response)=>{
-    const data=JSON.parse(response.body);
-    console.log(data.location)
+
+//challenge one
+request({url:url,json:true},(error,response)=>{
+    const data=response.body.current;
+    console.log(`It is currently ${data.temp_c} degrees out.And feels like ${data.feelslike_c} degrees `)
 })
+
+
+

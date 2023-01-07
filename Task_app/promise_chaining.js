d2746547b8d4caa0773dd8efcc2f853fd347ca40
pabina <rimalpabina1234@gmail.com>
2023-01-08 01:38:34 +0545

    promish chaining to async_await challenge

--- Task_app/promise_chaining.js
+++ Task_app/promise_chaining.js
@@ -2,13 +2,27 @@ const Task=require("./src/models/mytask.js");
 require("./src/db.js");
 
 
-Task.findByIdAndRemove("63b9342270f69c52d4a2d79e").then((result1)=>{
-console.log(result1)
-return Task.countDocuments({completed:true})
-}).then((result2)=>{
-console.log(result2)
-}).catch((error)=>{
-    console.log(error)
-})
+// Task.findByIdAndRemove("63b9342270f69c52d4a2d79e").then((result1)=>{
+// console.log(result1)
+// return Task.countDocuments({completed:true})
+// }).then((result2)=>{
+// console.log(result2)
+// }).catch((error)=>{
+//     console.log(error)
+// })
 
 //modifying promise chaining to async await
+
+const RemoveAndCount=async(id)=>{
+    let removeData= await Task.findByIdAndRemove(id);
+    let CountsameMatch=await Task.countDocuments({completed:true})
+    return {removeData,CountsameMatch};
+}
+
+RemoveAndCount("63b930aeb9173692f32f4621").then(({removeData,CountsameMatch})=>{
+console.log(removeData)
+console.log(CountsameMatch)
+
+}).catch((e)=>{
+console.log(e)
+})

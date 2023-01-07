0adb7ec1014f9637cb29522b1aa4a7860246f95b
pabina <rimalpabina1234@gmail.com>
2023-01-07 22:21:33 +0545

    promise chainning challenge

---
+++ Task_app/promise_chaining.js
@@ -0,0 +1,12 @@
+const Task=require("./src/models/mytask.js");
+require("./src/db.js");
+
+
+Task.findByIdAndRemove("63b9342270f69c52d4a2d79e").then((result1)=>{
+console.log(result1)
+return Task.countDocuments({completed:true})
+}).then((result2)=>{
+console.log(result2)
+}).catch((error)=>{
+    console.log(error)
+})

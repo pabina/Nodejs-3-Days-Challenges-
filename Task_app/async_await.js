3689194ce63e9170c7e0f954eaa4d778569afd82
pabina <rimalpabina1234@gmail.com>
2023-01-08 01:05:32 +0545

    async_await practice 2

--- Task_app/async_await.js
+++ Task_app/async_await.js
@@ -2,16 +2,38 @@
 
 
 
+
 //async await function
 const add=async(a,b)=>{
-  return a+b
- 
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+       resolve(a+b) 
+    }, 200);
+  })
 }
-add(2,3).then((sum)=>{
-console.log(sum)
-}).catch((e)=>{
-    console.log(e)
+
+
+const asyncfun=async()=>{
+const sumfirst =await add(2,3);
+const sumsencond = await add(sumfirst,8);
+const sumthird=await  add(sumsencond,10);
+return sumthird;
+}
+
+asyncfun().then((result)=>{
+console.log(result)
+}).catch((error)=>{
+    console.log(error)
 })
+
+
+
+
+// add(2,3).then((sum)=>{
+// console.log(sum)
+// }).catch((e)=>{
+//     console.log(e)
+// })
 
 
 // add(2,3).then((sum)=>{

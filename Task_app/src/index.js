87e16dd82d00331a42711b2dee2a64f40065711b
pabina <rimalpabina1234@gmail.com>
2023-01-07 15:43:09 +0545

    fetchding data from model

--- Task_app/src/index.js
+++ Task_app/src/index.js
@@ -7,10 +7,10 @@ const app=express();
 app.use(express.json())
 
 
-app.get("/mytask",(req,res)=>{
-    res.send("you are welcome");
-})
 
+
+
+//for create
 app.post("/mytask",(req,res)=>{
     const MytaskData=new TASKMODEL(req.body)
     MytaskData.save().then(()=>{
@@ -20,6 +20,31 @@ app.post("/mytask",(req,res)=>{
    res.status(400).send(error)
     })
 //    res.send(req.body)
+})
+
+
+//for reading
+app.get("/mytask",(req,res)=>{
+     TASKMODEL.find().then((result)=>{
+res.status(200).send(result);
+    }).catch((error)=>{
+        res.status(500).send(error)
+    })
+    
+})
+
+//for reading single item
+app.get("/mytask/:id",(req,res)=>{
+    const _id=req.params.id;
+    
+   TASKMODEL.findById(_id).then((result)=>{
+    if(!result){
+       return res.status(404).send()
+    }
+    res.status(200).send(result)
+   }).catch((error)=>{
+    res.status(500).send(error)
+   })
 })
 
 

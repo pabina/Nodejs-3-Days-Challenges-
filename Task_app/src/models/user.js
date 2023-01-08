1c194003a6d350b354e988b0aab51f3f632dafca
pabina <rimalpabina1234@gmail.com>
2023-01-08 23:35:02 +0545

    hash password done with middleware

--- Task_app/src/models/user.js
+++ Task_app/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
-const validator=require("validator")
+const validator=require("validator");
+const bcrypt=require("bcryptjs")
 
 
 
@@ -36,8 +37,12 @@ const userSchema=mongoose.Schema({
 
 })
 
-userSchema.pre("save",function(next){
-  console.log("run before save");
+userSchema.pre("save",async function(next){
+  const user=this;
+
+if(user.isModified("password")){
+  user.password=await bcrypt.hash(user.password,8);
+}
   next();
 })
 

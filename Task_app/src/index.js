da34cc355b9d6858c7611396fd410f7d46994831
pabina <rimalpabina1234@gmail.com>
2023-01-08 10:53:30 +0545

    seperate routes added

--- Task_app/src/index.js
+++ Task_app/src/index.js
@@ -1,116 +1,11 @@
 const express=require("express");
-const TASKMODEL=require("./models/mytask.js");
+let taskroute=require("./routes/taskroute.js")
 
 require("./db.js");
 
 const app=express();
 app.use(express.json())
-
-
-//using router
-const router=express.Router();
-router.get("/router",async(req,res)=>{
-    res.send("this is the router path")
-})
-app.use(router);
-
-
-//for create
-app.post("/mytask",async(req,res)=>{
-    const MytaskData=new TASKMODEL(req.body)
-
-try {
-  await MytaskData.save();
-  res.status(201).send(MytaskData)
-} catch (error) {
-    res.status(400).send(error)  
-}
-    
-//     MytaskData.save().then(()=>{
-//     console.log(MytaskData)
-//     res.status(201).send(MytaskData)
-//     }).catch((error)=>{
-//    res.status(400).send(error)
-//     })
-})
-
-
-//for reading
-app.get("/mytask",async(req,res)=>{
-    try {
-     let alltask= await TASKMODEL.find({});
-        res.status(200).send(alltask);
-    } catch (error) {
-        res.status(500).send(error) 
-    }
-//      TASKMODEL.find({}).then((result)=>{
-// res.status(200).send(result);
-//     }).catch((error)=>{
-//         res.status(500).send(error)
-//     })
-    
-})
-
-
-
-//for reading single item
-app.get("/mytask/:id",async(req,res)=>{
-    const _id=req.params.id;
-
-    try {
-    let taskis=  await TASKMODEL.findById(_id) 
-    if(!taskis){
-        return res.status(404).send("not found any task") 
-    }
-    res.status(200).send(taskis)  
-    } catch (error) {
-        res.status(500).send(error)   
-    }
-
-//    TASKMODEL.findById(_id).then((result)=>{
-//     if(!result){
-//        return res.status(404).send()
-//     }
-//     res.status(200).send(result)
-//    }).catch((error)=>{
-//     res.status(500).send(error)
-//    })
-})
-
-
-//for update
-app.patch("/mytask/:id",async(req,res)=>{
-    let updates=Object.keys(req.body);
-    let validateskeys=["taskname","description","completed"];
-    let updatevalidation=updates.every((updatedata)=>validateskeys.includes(updatedata))
-    if(!updatevalidation){
-        return res.status(404).send("not matching key")
-    }
-    try {
-        let updatetask=await TASKMODEL.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
-      if(!updatetask){
-        res.status(404).send("not found any task")
-      }
-      res.send(updatetask)
-      
-    } catch (error) {
-      res.send(error) 
-    }
-})
-
-
-// for delete
-app.delete("/mytask/:id",async(req,res)=>{
-    try {
-        let task=await TASKMODEL.findByIdAndDelete(req.params.id);
-        if(!task){
-            res.status(404).send("task not found")
-        }
-        res.send(task)
-    } catch (error) {
-       res.send(error) 
-    }
-})
+app.use(taskroute);
 
 
 
---
+++ Task_app/src/routes/taskroute.js
@@ -0,0 +1,88 @@
+const express=require('express');
+let TASKMODEL=require("../models/mytask.js")
+
+const router=express.Router();
+
+
+//for create
+router.post("/mytask",async(req,res)=>{
+    const MytaskData=new TASKMODEL(req.body)
+
+try {
+  await MytaskData.save();
+  res.status(201).send(MytaskData)
+} catch (error) {
+    res.status(400).send(error)  
+}
+    
+
+})
+
+
+//for reading
+router.get("/mytask",async(req,res)=>{
+    try {
+     let alltask= await TASKMODEL.find({});
+        res.status(200).send(alltask);
+    } catch (error) {
+        res.status(500).send(error) 
+    }
+    
+})
+
+
+
+//for reading single item
+router.get("/mytask/:id",async(req,res)=>{
+    const _id=req.params.id;
+
+    try {
+    let taskis=  await TASKMODEL.findById(_id) 
+    if(!taskis){
+        return res.status(404).send("not found any task") 
+    }
+    res.status(200).send(taskis)  
+    } catch (error) {
+        res.status(500).send(error)   
+    }
+
+})
+
+
+//for update
+router.patch("/mytask/:id",async(req,res)=>{
+    let updates=Object.keys(req.body);
+    let validateskeys=["taskname","description","completed"];
+    let updatevalidation=updates.every((updatedata)=>validateskeys.includes(updatedata))
+    if(!updatevalidation){
+        return res.status(404).send("not matching key")
+    }
+    try {
+        let updatetask=await TASKMODEL.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+      if(!updatetask){
+        res.status(404).send("not found any task")
+      }
+      res.send(updatetask)
+      
+    } catch (error) {
+      res.send(error) 
+    }
+})
+
+
+// for delete
+router.delete("/mytask/:id",async(req,res)=>{
+    try {
+        let task=await TASKMODEL.findByIdAndDelete(req.params.id);
+        if(!task){
+            res.status(404).send("task not found")
+        }
+        res.send(task)
+    } catch (error) {
+       res.send(error) 
+    }
+})
+
+
+
+module.exports=router;

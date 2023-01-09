be0613c09c6bde158756a6e3c490edf9a9e15963
pabina <rimalpabina1234@gmail.com>
2023-01-09 12:25:31 +0545

    jwt intro practice

--- Task_app/src/index.js
+++ Task_app/src/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const taskroute=require("./routes/taskroute.js")
 const userroute=require("./routes/userroute.js")
+const jwt=require("jsonwebtoken");
 
 require("./db.js");
 
@@ -10,23 +11,22 @@ app.use(taskroute);
 app.use(userroute);
 
 
+ 
 
 
 
 
-
-// const hashFun=async()=>{
-//     let password="pabina"
-//     let hashpassword= await bcrypt.hash(password,8);
-//     console.log(hashpassword)
-//     let result= await bcrypt.compare(password,hashpassword);
-//     console.log(result)
-// console.log("this is for hashfun");
-// }
-
-// hashFun();
+//generating authentication token
+const authenticationFun=()=>{
+  const token=  jwt.sign({_id:"hellopabina123456"}, "mysecretekey",{expiresIn:"1 s"});
+  console.log(token)
+  const data=jwt.verify(token,"mysecretekey")
+  console.log(data)
 
 
+}
+
+authenticationFun();
 
 
 

2d59c1efa82e76974180a30764ce4e7893ead3d2
pabina <rimalpabina1234@gmail.com>
2023-01-11 00:06:09 +0545

    include timestamps feature

--- Task_app/src/models/mytask.js
+++ Task_app/src/models/mytask.js
@@ -1,29 +1,60 @@
 const mongoose=require("mongoose")
 
+const TASKSchema=mongoose.Schema({
 
-const TASKMODEL=mongoose.model("Task",{
     taskname:{
-  type:String,
-    required:true
-    },
-    description:{
         type:String,
-        trim:true,
-        lowercase:true,
-        required:true
-        
-    },
-    completed:{
-        type:Boolean,
-        default:false,
-        required:true
-    },
+          required:true
+          },
+          description:{
+              type:String,
+              trim:true,
+              lowercase:true,
+              required:true
+              
+          },
+          completed:{
+              type:Boolean,
+              default:false,
+              required:true
+          },
+      
+         owner:{
+          type:mongoose.Schema.Types.ObjectId,
+          require:true,
+          ref:"user"
+          }
 
-   owner:{
-    type:mongoose.Schema.Types.ObjectId,
-    require:true,
-    ref:"user"
-    }
-});
+},{
+    timestamps:true
+})
+
+
+// const TASKMODEL=mongoose.model("Task",{
+//     taskname:{
+//   type:String,
+//     required:true
+//     },
+//     description:{
+//         type:String,
+//         trim:true,
+//         lowercase:true,
+//         required:true
+        
+//     },
+//     completed:{
+//         type:Boolean,
+//         default:false,
+//         required:true
+//     },
+
+//    owner:{
+//     type:mongoose.Schema.Types.ObjectId,
+//     require:true,
+//     ref:"user"
+//     }
+// });
+
+const TASKMODEL=mongoose.model("Task",TASKSchema);
 
 module.exports=TASKMODEL;
--- Task_app/src/models/user.js
+++ Task_app/src/models/user.js
@@ -46,6 +46,8 @@ const userSchema=mongoose.Schema({
   }
  }]
 
+},{
+  timestamps:true
 })
 
 

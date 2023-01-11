93fc11eb837a83e4591a56564a886df9f69f52cf
pabina <rimalpabina1234@gmail.com>
2023-01-11 10:27:51 +0545

    introduce to multer

--- Task_app/src/index.js
+++ Task_app/src/index.js
@@ -2,13 +2,14 @@ const express=require("express");
 const taskroute=require("./routes/taskroute.js")
 const userroute=require("./routes/userroute.js")
 const jwt=require("jsonwebtoken");
-const auth=require("./middleware/auth.js");
-const TASKMODEL=require("./models/mytask.js");
-const UserModel=require("./models/user.js");
+
+
 
 require("./db.js");
 
 const app=express();
+
+const multer=require("multer")
 
 
 
@@ -17,17 +18,22 @@ app.use(taskroute);
 app.use(userroute);
 
 
- 
-// app.get("/task",auth,async(req,res)=>{
-// const getalltask= await TASKMODEL.find({owner:req.user._id});
-// console.log(getalltask[0].owner)
-// res.send(getalltask);
-// })
-
-app.get("/alltask",async(req,res)=>{
-const alltask=await TASKMODEL.find({}, "",{limit:2,sort:{"taskname":-1}})
-res.send(alltask);
+const upload=multer({
+    dest:"images"
 })
+
+app.post("/upload",upload.single("upload"),async(req,res)=>{
+res.send("upload successfully");
+})
+
+
+ 
+
+
+// app.get("/alltask",async(req,res)=>{
+// const alltask=await TASKMODEL.find({}, "",{limit:2,sort:{"taskname":-1}})
+// res.send(alltask);
+// })
 
 
 

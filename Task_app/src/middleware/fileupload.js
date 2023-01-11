4751c215239bd4a92f2e609d5d4201a9f4cec936
pabina <rimalpabina1234@gmail.com>
2023-01-11 15:11:25 +0545

    validation file upload using limi and filefilter

--- Task_app/src/middleware/fileupload.js
+++ Task_app/src/middleware/fileupload.js
@@ -1,7 +1,16 @@
 const multer=require("multer")
 
 const upload=multer({
-    dest:"avatars"
+    dest:"avatars",
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.endsWith(".pdf")){
+           return cb(new Error("please upload PDF file"))
+        }
+        cb(undefined,true)
+    }
 })
 
 

4db2eb35bc239a07837893af166b3f723c3413da
pabina <rimalpabina1234@gmail.com>
2023-01-13 14:31:26 +0545

    challenge one of test case done

--- Task_app/src/math.js
+++ Task_app/src/math.js
@@ -1,11 +1,21 @@
-//calculate tip
+// //calculate tip
 
-const calculateTip=(total,tippercent=10)=>{
-// const tip =(total*(tippercent/100)) +total
-const tip =(total*(tippercent/100)) 
-return tip;
+// const calculateTip=(total,tippercent=10)=>{
+// // const tip =(total*(tippercent/100)) +total
+// const tip =(total*(tippercent/100)) 
+// return tip;
+// }
+
+
+//challenges test
+const FahrenheitToCelsius=(temp)=>{
+    return(temp-32)/1.8
 }
 
-module.exports={calculateTip};
+const CelsiusToFahrenheit=(temp)=>{
+    return (temp * 1.8)+32
+}
+module.exports={FahrenheitToCelsius,CelsiusToFahrenheit}
 
-// console.log(calculateTip(300,20))
+
+// module.exports={calculateTip};
--- Task_app/src/test/math.test.js
+++ Task_app/src/test/math.test.js
@@ -1,15 +1,31 @@
-const {calculateTip} = require(".././math.js")
+// const {calculateTip} = require(".././math.js")
+const {FahrenheitToCelsius,CelsiusToFahrenheit}=require("../math.js");
+
 
 
 
  //math test
- test("math test",()=>{
-   const totaltips= calculateTip(200,30);
-   expect(totaltips).toBe(60)
- });
+//  test("math test",()=>{
+//    const totaltips= calculateTip(200,30);
+//    expect(totaltips).toBe(60)
+//  });
+
 
 //math default test
-test("math default test",()=>{
-    const totaltips= calculateTip(200);  
-    expect(totaltips).toBe(20) 
+// test("math default test",()=>{
+//     const totaltips= calculateTip(200);  
+//     expect(totaltips).toBe(20) 
+// })
+
+//challenge one
+
+test("fahrenheittocelsius test",()=>{
+  const CelsiusTemp= FahrenheitToCelsius(32);
+  expect(CelsiusTemp).toBe(0)
 })
+
+
+test("celsiustofahrenheit test",()=>{
+    const FahrenheitTemp= CelsiusToFahrenheit(0);
+    expect(FahrenheitTemp).toBe(32)
+  })
